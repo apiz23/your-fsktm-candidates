39aced70d392cc092c3c7a3b54d6f3eb0f8636d9
Hafizu <piz230601@gmail.com>
2024-12-20 19:46:51 +0800

    update creator

--- app/creator/page.tsx
+++ app/creator/page.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import Link from "next/link";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import TechVentura from "@/public/images/tech-ventura.png";
 
 export default function Creator() {
 	return (
@@ -12,7 +14,6 @@ export default function Creator() {
 					DI230052 | AA210392 | MJ195345 | JG160007
 				</p>
 			</div>
-
 			<div className="grid grid-cols-2 gap-8 mt-10">
 				<div className="flex flex-col items-center text-center">
 					<Link
@@ -37,6 +38,19 @@ export default function Creator() {
 					</Link>
 					<p className="mt-2 text-lg">LinkedIn</p>
 				</div>
+			</div>
+			<div className="flex flex-col items-center pt-6">
+				<Image
+					src={TechVentura}
+					alt="Tech Ventura Logo"
+					className="h-12 w-12"
+					width={48}
+					height={48}
+				/>
+				<p className="mt-2 text-md font-semibold text-gray-600">Tech Ventura</p>
+				<p className="mt-1 text-xs text-gray-600">
+					© 2024 Tech Ventura. All rights reserved.
+				</p>
 			</div>
 		</div>
 	);
